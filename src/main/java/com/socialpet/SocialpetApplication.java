00f128199a57214c130446f6466d00f0e2db408c
Henrique Sá <hinriqui@outlook.com.br>
2025-06-02 11:08:32 -0300

    first commit

---
+++ src/main/java/com/socialpet/SocialpetApplication.java
@@ -0,0 +1,13 @@
+package com.socialpet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialpetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialpetApplication.class, args);
+	}
+
+}
